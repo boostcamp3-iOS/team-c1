e850a7060527f38257d3c929848fa1f59eda0515
Young Jun <0jun0815@gmail.com>
2019-01-22 10:38:45 +0900

    프로젝트 생성

---
+++ CoCo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoCo
+//
+//  Created by 최영준 on 22/01/2019.
+//  Copyright © 2019 Team CoCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
