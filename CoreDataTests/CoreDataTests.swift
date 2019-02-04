fafaca8ba2f7de8de910f9c2858d369f39e478d7
JunyoungKang <young16505@gmail.com>
2019-02-04 21:41:14 +0900

    Add MockManager, UnitTest Code

--- CoreDataTests/CoreDataTests.swift
+++ CoreDataTests/CoreDataTests.swift
@@ -13,44 +13,139 @@ import CoreData
 class MockSearchWordCoreDataManager: SearchWordCoreDataManagerType {
     var mockSearchWordCoreData = [SearchWordData]()
     var mockSearchWord: [String]?
+    var mockSearchInfo = MockSearchWordInfo()
     
     @discardableResult func insert<T: CoreDataStructEntity>(_ coreDataStructType: T) throws -> Bool{
-        let beforeCount = mockSearchWordCoreData.count
-        let  afterCount = mockSearchWordCoreData.count
+        let beforeCount = mockSearchInfo.mockSearchWord.count
         guard let coreDataStruct = coreDataStructType as? SearchWordData else {
             return false
         }
-        mockSearchWordCoreData.append(coreDataStruct)
+        print(coreDataStruct)
+        mockSearchInfo.mockSearchWord.append(coreDataStruct)
+        let afterCount = mockSearchInfo.mockSearchWord.count
         return beforeCount + 1 == afterCount ? true : false
     }
     
-    func fetchOnlySearchWord() throws -> [String]? {
-        return MockSearchWordInfo.mockSearchWord
+    func fetchOnlySearchWord(pet: String) throws -> [String]? {
+        let result = mockSearchInfo.mockSearchWord.filter{ $0.searchWord.contains(pet)
+        }
+        var stringResult = [String]()
+        for value in result {
+            stringResult.append(value.searchWord)
+        }
+        return stringResult
     }
     
-    func fetchObjects() throws -> [CoreDataStructEntity]? {
-        return nil
+    func fetchObjects(pet: String) throws -> [CoreDataStructEntity]? {
+        let result = mockSearchInfo.mockSearchWord.filter{ $0.searchWord.contains(pet)
+        }
+        return result
     }
     
     func updateObject<T>(_ coreDataStructType: T) throws -> Bool {
+        guard let searchWordData = coreDataStructType as? SearchWordData else {
+            return false
+        }
+        let mockSearchWords = mockSearchInfo.mockSearchWord
+        let object = mockSearchWords.filter { (searchWord) -> Bool in
+            if searchWord.searchWord == searchWordData.searchWord {
+                return true
+            }
+            return false
+        }
+        guard var result = object.first else { return false }
+        result.date = searchWordData.date
         return false
     }
     
+    @discardableResult func updateObject(with searchWord: String, pet: String) throws -> Bool {
+        var result = false
+        do {
+            guard let objects = try fetchObjects(pet: pet) as? [SearchWordData] else { return false }
+            let object = objects.filter { $0.searchWord == searchWord }
+            if var first = object.first {
+                first.date = mockSearchInfo.createDate()
+                mockSearchInfo.mockSearchWord.append(first)
+                let newResult = mockSearchInfo.mockSearchWord
+                print("mockCount: \(mockSearchInfo.mockSearchWord.count)")
+                result = true
+            }
+        }
+        return result
+    }
+    
     func deleteObject<T>(_ coreDataStructType: T) throws -> Bool {
+        guard let searchWordData = coreDataStructType as? SearchWordData else {
+            return false
+        }
+        var index = 0
+        for object in mockSearchInfo.mockSearchWord {
+            if object.objectID == searchWordData.objectID {
+                break
+            }
+            index = index + 1
+        }
+        mockSearchInfo.mockSearchWord.remove(at: index)
+        return false
+    }
+    
+    @discardableResult func deleteAllObjects(pet: String) throws ->  Bool {
+        let nonDeleteObjects = mockSearchInfo.mockSearchWord.filter { $0.pet
+            != pet }
+        mockSearchInfo.mockSearchWord.removeAll()
+        mockSearchInfo.mockSearchWord = nonDeleteObjects
+        print(mockSearchInfo.mockSearchWord)
+        do {
+            guard let result = try fetchObjects(pet: pet) else { return false }
+            if result.count == 0 {
+                return true
+            }
+        }
+    
         return false
     }
 }
 
 class MockSearchWordInfo {
-    static let mockSearchWord = ["강아지 옷", "강아지 간식", "강아지 사료", "고양이 옷"]
+    var mockSearchWord: [SearchWordData] = {
+        var searchWordDatas = [SearchWordData]()
+        var searchWordData1 = SearchWordData()
+        searchWordData1.date = searchWordData1.createDate()
+        searchWordData1.searchWord = "고양이 옷"
+        searchWordData1.pet = "고양이"
+       // searchWordData1.objectID = NSManagedObjectID()
+        var searchWordData2 = SearchWordData()
+        searchWordData2.date = searchWordData2.createDate()
+        searchWordData2.searchWord = "고양이 장난감"
+        searchWordData2.pet = "고양이"
+       // searchWordData2.objectID = NSManagedObjectID()
+        var searchWordData3 = SearchWordData()
+        searchWordData3.date = "2019-01-02"
+        searchWordData3.searchWord = "강아지 장난감"
+        searchWordData3.pet = "강아지"
+       // searchWordData3.objectID = NSManagedObjectID()
+        
+        searchWordDatas.append(searchWordData1)
+        searchWordDatas.append(searchWordData2)
+        searchWordDatas.append(searchWordData3)
+        return searchWordDatas
+    }()
+    
+    func createDate() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return dateFormatter.string(from: date)
+    }
 }
 
 class MockPetKeywordCoreDataManager: PetKeywordCoreDataManagerType {
-    func fetchOnlyKeyword() throws -> [String]? {
+    func fetchOnlyKeyword(pet: String) throws -> [String]? {
         return nil
     }
     
-    func fetchOnlyPet() throws -> String? {
+    func fetchOnlyPet(pet: String) throws -> String? {
         return nil
     }
     
@@ -58,7 +153,7 @@ class MockPetKeywordCoreDataManager: PetKeywordCoreDataManagerType {
         return false
     }
     
-    func fetchObjects() throws -> [CoreDataStructEntity]? {
+    func fetchObjects(pet: String) throws -> [CoreDataStructEntity]? {
          return nil
     }
     
@@ -72,11 +167,11 @@ class MockPetKeywordCoreDataManager: PetKeywordCoreDataManagerType {
 }
 
 class MockMyGoodsCoreDataManager: MyGoodsCoreDataManagerType {
-    func fetchFavoriteGoods() throws -> [MyGoodsData]? {
+    func fetchFavoriteGoods(pet: String) throws -> [MyGoodsData]? {
          return nil
     }
     
-    func fetchLatestGoods() throws -> [MyGoodsData]? {
+    func fetchLatestGoods(pet: String) throws -> [MyGoodsData]? {
          return nil
     }
     
@@ -84,7 +179,7 @@ class MockMyGoodsCoreDataManager: MyGoodsCoreDataManagerType {
         return false
     }
     
-    func fetchObjects() throws -> [CoreDataStructEntity]? {
+    func fetchObjects(pet: String) throws -> [CoreDataStructEntity]? {
         return nil
     }
     
@@ -104,6 +199,7 @@ class CoreDataTests: XCTestCase {
     let mockSearchWordCoreDataManager = MockSearchWordCoreDataManager()
     let mockPetKeywordCoreDataManager = MockPetKeywordCoreDataManager()
     let mockNWManager = MockShoppingNetworkManager()
+    var mockSearchInfo = MockSearchWordInfo()
     
     lazy var service = SearchService(serachCoreData: mockSearchWordCoreDataManager,
                                      petCoreData: mockPetKeywordCoreDataManager,
@@ -118,35 +214,65 @@ class CoreDataTests: XCTestCase {
     
     func testFetchCoreData() {
         // given
-        mockSearchWordCoreDataManager.mockSearchWord = MockSearchWordInfo.mockSearchWord
+        let pet = "강아지"
         
         // when
-        let result = service.fetchRecentSearchWord()
+        let result = service.fetchRecentSearchWord(pet: pet)
         
+        let count = mockSearchInfo.mockSearchWord.filter{ $0.searchWord.contains(pet)
+        }.count
         // then
-        XCTAssert(result!.count == MockSearchWordInfo.mockSearchWord.count , "Fetch Fail")
+        XCTAssert(result!.count == count , "Fetch Fail")
     }
     
     func testInsert() {
+        let pet = "강아지"
+        let searchWord = "강아지 한복"
+    
         // given
-        service.insert(recenteSearchWord: "고양이 옷")
+        service.insert(recenteSearchWord: searchWord, pet: pet)
         do {
             // when
-            let result = try mockSearchWordCoreDataManager.fetchOnlySearchWord()
+            let result = try mockSearchWordCoreDataManager.fetchOnlySearchWord(pet: pet)
             guard let nonOpResult = result else { return }
+            print(nonOpResult)
             //then
-            XCTAssert(nonOpResult.contains("고양이 옷"), "InsertFail")
+            XCTAssert(nonOpResult.contains(searchWord), "InsertFail")
         } catch let error {
             print(error)
         }
     }
     
     func testUpdateCoreData() {
-        
+        let pet = "강아지"
+        let searchWord = "강아지 장난감"
+        let beforeCount = mockSearchInfo.mockSearchWord.filter { $0.pet == pet }.count
+        //given
+        service.updateRecentSearchWord(searchWord: searchWord, pet: pet)
+        do {
+            // when
+            guard let result = try mockSearchWordCoreDataManager.fetchObjects(pet: pet) as? [SearchWordData] else { return }
+            // then : 원래 로직은 기존의 값을 업데이트 하지만 테스트는 같은 값을 하나 더 넣기 때문에 카운트로 비교
+             XCTAssert(beforeCount != result.count, "Update Fail")
+        } catch let error {
+            print(error)
+        }
     }
     
     func testDeleteCoreData() {
-        
+        let pet = "강아지"
+        //given
+        let result = service.deleteRecentSearchWord(pet: pet)
+        var count = 0
+        //when
+        do {
+            guard let object = try mockSearchWordCoreDataManager.fetchObjects(pet: pet) else { return }
+            count = object.count
+        } catch let error {
+            print(error)
+        }
+        //then
+        XCTAssert(count == 0, "Delete Fail")
     }
 
     func testPerformanceExample() {
