bd1683b52fbcb0ce127eb2b981feccdfcc20a81f
Young Jun <0jun0815@gmail.com>
2019-02-09 00:11:46 +0900

    Create WebViewControllerError enum, getErrorMessage(_:) method in WebViewController

--- CoCo/WebViewController/WebViewController.swift
+++ CoCo/WebViewController/WebViewController.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2019 Team CoCo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class WebViewController: UIViewController {
+}
+
+// MARK: - ErrorHandlerType
+extension WebViewController: ErrorHandlerType {
+    func getErrorMessage(_ error: ErrorType) -> String {
+        guard let webVCError = error as? WebViewControllerError else {
+            return error.unknownError
+        }
+        return webVCError.rawValue
+    }
+}
