7e8899021e2b6b5c6eb7ec757b1455a642b91f0a
Young Jun <0jun0815@gmail.com>
2019-02-16 18:09:59 +0900

    Update Algorithm

--- CoCo/Algorithm/Algorithm.swift
+++ CoCo/Algorithm/Algorithm.swift
@@ -33,9 +33,11 @@ class Algorithm: AlgorithmType {
             }
         }
         for keyword in keywords {
-
+            for category in keyword.getData() {
+                result += category.getData(pet: pet)
+            }
         }
-        return []
+        return result
     }
 }
 
@@ -168,7 +170,7 @@ extension Algorithm {
      - Parameter pet: 강아지/고양이
      - Parameter words: 결합시킬 단어 리스트
      */
-    func combinePet(_ pet: String, and words: [String]) -> [String] {
+    func combinePet(_ pet: Pet, and words: [String]) -> [String] {
         var result = [String]()
         for word in words {
             result.append(combinePet(pet, and: word))
@@ -183,8 +185,8 @@ extension Algorithm {
      - Parameter pet: 강아지/고양이
      - Parameter word: 결합시킬 단어
      */
-    func combinePet(_ pet: String, and word: String) -> String {
-        return "\(pet) " + word
+    func combinePet(_ pet: Pet, and word: String) -> String {
+        return "\(pet.rawValue) " + word
     }
     /**
      문자열 리스트에서 동물(강아지, 고양이) 단어를 제거한다.
