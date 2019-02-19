26fc664abda6b1a250bd9fa01d419ff165240815
이호찬 <1ghcks@gmail.com>
2019-02-19 14:23:09 +0900

    Create SettingPetKeywordView

---
+++ CoCo/SettingView/SettingMyPet/SettingPetAndKeywordService.swift
@@ -0,0 +1,43 @@
+//
+//  SettingMyPetService.swift
+//  CoCo
+//
+//  Created by 이호찬 on 18/02/2019.
+//  Copyright © 2019 Team CoCo. All rights reserved.
+//
+
+import Foundation
+
+class SettingPetAndKeywordService {
+
+    private let petKeywordCoreDataManager: PetKeywordCoreDataManagerType
+
+    let petDic = [0: "강아지", 1: "고양이"]
+    let keywordDic = [0: "놀이", 1: "뷰티", 2: "헬스", 3: "푸드", 4: "스타일", 5: "리빙", 6: "외출", 7: "배변"]
+
+    var petKeyword: PetKeywordData?
+
+    init(petCoreData: PetKeywordCoreDataManagerType) {
+        petKeywordCoreDataManager = petCoreData
+    }
+
+    func fetchPetKeywordData() {
+        do {
+            if let petKeyword = try petKeywordCoreDataManager.fetchObjects(pet: nil)?.first as? PetKeywordData {
+                self.petKeyword = petKeyword
+                print(self.petKeyword)
+            }
+        } catch let err {
+            print(err)
+        }
+    }
+
+    func insertPetKeywordData() {
+        do {
+            _ = try petKeywordCoreDataManager.insert(PetKeywordData(pet: petKeyword?.pet ?? "강아지", keywords: petKeyword?.keywords ?? [""]))
+            print(petKeyword)
+        } catch let err {
+            print(err)
+        }
+    }
+}
---
+++ CoCo/SettingView/SettingMyPet/SettingPetAndKeywordTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SettingPetAndKeywordTableViewCell.swift
+//  CoCo
+//
+//  Created by 이호찬 on 19/02/2019.
+//  Copyright © 2019 Team CoCo. All rights reserved.
+//
+
+import UIKit
+
+class SettingPetAndKeywordTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var petKeywordLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
