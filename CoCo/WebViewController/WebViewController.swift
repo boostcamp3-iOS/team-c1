f61e391194cae4f68e35e19b7bf7cde6a43fec59
Young Jun <0jun0815@gmail.com>
2019-02-21 00:28:23 +0900

    Update WebVC

--- CoCo/WebViewController/WebViewController.swift
+++ CoCo/WebViewController/WebViewController.swift
@@ -17,18 +17,18 @@ class WebViewController: UIViewController {
     private let webViewKeyPaths = [
         #keyPath(WKWebView.estimatedProgress)
     ]
-
+    
     // MARK: - IBOutlets
     @IBOutlet private weak var webView: WKWebView!
     @IBOutlet private weak var progressView: UIView!
     @IBOutlet private weak var favoriteButton: UIBarButtonItem!
-
+    
     // MARK: - Required call methods
     // 웹 뷰 호출시 데이터를 전달한다. (필수)
     func sendData(_ data: MyGoodsData) {
         service = WebViewService(data: data)
     }
-
+    
     // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,7 +47,7 @@ class WebViewController: UIViewController {
         setProgressView()
         extendedLayoutIncludesOpaqueBars = true
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         service?.fetchData()
@@ -58,7 +58,7 @@ class WebViewController: UIViewController {
         setFavoriteButton()
         tabBarController?.tabBar.isHidden = true
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         if isFavorite != service?.myGoodsData.isFavorite {
@@ -66,22 +66,23 @@ class WebViewController: UIViewController {
         }
         tabBarController?.tabBar.isHidden = false
     }
-
+    
     // MARK: - Observer related methods
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
             updateProgressView(CGFloat(webView.estimatedProgress))
         }
     }
-
+    
     private func addObserversToWebView() {
         for keyPath in webViewKeyPaths {
             webView.addObserver(self, forKeyPath: keyPath, options: .new, context: nil)
         }
     }
-
+    
     // MARK: - Navigation related methods
     private func setNavigationBar() {
+        navigationController?.setNavigationBarHidden(false, animated: false)
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.prefersLargeTitles = false
         if let title = service?.myGoodsData.shoppingmall {
@@ -94,11 +95,11 @@ class WebViewController: UIViewController {
         navigationController?.interactivePopGestureRecognizer?.delegate = self
         navigationController?.interactivePopGestureRecognizer?.isEnabled = true
     }
-
+    
     @objc private func popViewController() {
         navigationController?.popViewController(animated: true)
     }
-
+    
     // MARK: - WebView related methods
     private func setWebView() {
         webView.navigationDelegate = self
@@ -120,39 +121,39 @@ class WebViewController: UIViewController {
         let request = URLRequest(url: url)
         webView.load(request)
     }
-
+    
     // MARK: - ProgressView related methods
     private func setProgressView() {
         progressView.backgroundColor = AppColor.purple
     }
-
+    
     private func updateProgressView(_ value: CGFloat) {
         progressView.frame.size.width = value * view.frame.width
     }
-
+    
     // MARK: - Button related methods
     private func setFavoriteButton() {
         favoriteButton.image = (isFavorite) ?
             UIImage(named: "like_fill") : UIImage(named: "like")
     }
-
+    
     // MARK: - Action methods
     @IBAction private func actionGoForward(_ sender: Any) {
         if webView.canGoForward {
             webView.goForward()
         }
     }
-
+    
     @IBAction private func actionGoBack(_ sender: Any) {
         if webView.canGoBack {
             webView.goBack()
         }
     }
-
+    
     @IBAction private func actionReload(_ sender: Any) {
         webView.reload()
     }
-
+    
     @IBAction private func actionShare(_ sender: Any) {
         guard let url = webView.url else {
             let message = getErrorMessage(MyGoodsDataError.invalidLink)
@@ -162,7 +163,7 @@ class WebViewController: UIViewController {
         let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: nil)
         present(activityVC, animated: true)
     }
-
+    
     @IBAction private func actionFavorite(_ sender: Any) {
         isFavorite = !isFavorite
         setFavoriteButton()
@@ -173,14 +174,25 @@ class WebViewController: UIViewController {
 extension WebViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
         // 웹뷰 로딩이 시작되면 progressView를 보여준다.
-        progressView.isHidden = false
-        view.bringSubviewToFront(progressView)
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {
+                return
+            }
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            self.progressView.isHidden = false
+            self.view.bringSubviewToFront(self.progressView)
+        }
     }
-
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         // 웹뷰 로딩이 안료되면 progressView를 숨긴다.
-        progressView.isHidden = true
-        navigationController?.setNavigationBarHidden(false, animated: false)
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {
+                return
+            }
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            self.progressView.isHidden = true
+        }
     }
 }
 
